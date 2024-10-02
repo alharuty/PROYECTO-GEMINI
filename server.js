592e0183ddff57d153a155bdf5a46064456f22f5
allharut <132786929+alharuty@users.noreply.github.com>
2024-10-02 12:19:54 +0200

    rehecho server.js

--- server.js
+++ server.js
@@ -1,17 +1,11 @@
 const express = require('express');
-const path = require('path'); // Asegurarse de usar path para rutas seguras
 const port = process.env.PORT || 8080;
 const app = express();
 
-// Sirve archivos estáticos desde el directorio 'dist'
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(__dirname + '/dist/'));
+app.get(/.*/, function (req, res) {
+  res.sendFile(__dirname + '/dist/index.html');
+})
+app.listen(port);
 
-// Redirige todas las rutas al archivo 'index.html' para que React Router maneje las rutas del lado del cliente
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
-
-// Iniciar el servidor
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+console.log("server started");
