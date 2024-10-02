33015d6e0508365c9e4b378bd61f8b60dd2c4eb6
allharut <132786929+alharuty@users.noreply.github.com>
2024-10-02 13:46:27 +0200

    vuelta a server.js original

--- server.js
+++ server.js
@@ -1,17 +1,11 @@
 const express = require('express');
-const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(__dirname + '/dist/'));
+app.get(/.*/, function (req, res) {
+  res.sendFile(__dirname + '/dist/index.html');
+})
+app.listen(port);
 
-// The "catchall" handler: for any request that doesn't
-// match one above, send back index.html.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
-
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
-});
+console.log("server started");
